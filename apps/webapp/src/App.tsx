364137d543e9fceac08b5f13c0a9cc29f85dc356
tan0s <tanujchamil@gmail.com>
2025-07-13 14:07:30 +0530

    fix: shadcn styles

--- apps/webapp/src/App.tsx
+++ apps/webapp/src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
+
 import './App.css'
 import { Button } from '@uprompt/ui/components/button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
   
-    <Button>
+    <Button variant="destructive">
         Click me
     </Button>
   )
