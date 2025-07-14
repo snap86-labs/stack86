bd855d80a238133661e709b4322b6fec2f90ba6f
tan0s <tanujchamil@gmail.com>
2025-07-14 16:34:52 +0530

    add: wormhole

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -1,23 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [ tanstackRouter({
+export default ({ mode }: any) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
-    }),react(),tailwindcss()],
-  build: {
-    outDir: resolve(__dirname, '../webapi/.dist'),
-    emptyOutDir: true
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8787',
+    }), react(), tailwindcss()],
+    build: {
+      outDir: resolve(__dirname, '../webapi/.dist'),
+      emptyOutDir: true
+    },
+    server: {
+      proxy: {
+        '/api': process.env.VITE_DEV_API_URL as string,
       }
     }
   }
-)
+  )
+}
